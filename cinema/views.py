bb6ba35fa1602b951e20fe145452f3ee91925c76
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-10-03 21:30:18 +0300

    removed legacy code

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from logging import raiseExceptions
 
 from django.db.models import F, Count
 from rest_framework import viewsets, mixins, status
@@ -112,11 +113,9 @@ class MovieViewSet(
     def upload_image(self, request, pk=None):
         movie = self.get_object()
         serializer = self.get_serializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
